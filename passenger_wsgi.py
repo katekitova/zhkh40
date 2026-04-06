7c9519a5927a8aa29ea6b1865910bf3e5445c7f4
Andrew <xiarumimeia@gmail.com>
2026-04-06 13:38:41 +0300

    +админка, так же изменины страницы "О нас", Главная и чат

---
+++ passenger_wsgi.py
@@ -0,0 +1,10 @@
+﻿import sys
+import os
+
+INTERP = os.path.expanduser("/var/www/u3471892/data/flaskenv/bin/python")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.insert(0, os.getcwd())
+
+from app import app as application
